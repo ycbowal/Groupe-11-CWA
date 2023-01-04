d192464aff4ae98cf3a490d8a2171750fe3960fe
belloaltine5279 <32312772+belloaltine5279@users.noreply.github.com>
2023-01-04 14:44:30 +0100

    Update product.ts

--- Projet/src/app/model/product.ts
+++ Projet/src/app/model/product.ts
@@ -6,22 +6,8 @@ export class Product {
     realPrice!: number;
     totalNbCopies!: number;
     urlMainImage!: string;
-    typeofproducts!: string; 
     isAvailable = true;
-  constructor(
-     realtiveprice : number , id: string, name : string , description : string, 
-     realprice : number, totalnbcopies: number, urlimage : string, 
-     typeofproducts : string , isavailable: boolean 
-  ) {
-    this.relativePrice = realtiveprice ; 
-    this.id = id ; 
-    this.name = name ; 
-    this.description = description ; 
-    this.realPrice = realprice ; 
-    this.totalNbCopies = totalnbcopies ; 
-    this.urlMainImage = urlimage ; 
-    this.typeofproducts = typeofproducts ; 
-    this.isAvailable = isavailable ; 
+  constructor() {
   }
  
 }
